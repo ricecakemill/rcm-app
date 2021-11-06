9f427a827aa657c39f91b7f9c8493940d5b38c72
Hanul <hanul@hanul.me>
2021-11-06 21:44:26 +0900

    곳간 추가

--- src/main.ts
+++ src/main.ts
@@ -156,6 +156,13 @@ import Wallet from "./klaytn/Wallet";
       })
     ),
 
+    el("h2", "떡방앗간 곳간"),
+    el(
+      "p",
+      "곳간은 떡방앗간 코인 이야기의 자산들이 보관된 곳입니다.\n어디에도 주거나 팔지 않고 영원히 간직됩니다."
+    ),
+    el("a", "곳간 주소\nhttps://opensea.io/0x20bF4fc7376D9Ec2E9Fd0aB182DD2Ac066Ce3AF7", { href: "https://opensea.io/0x20bF4fc7376D9Ec2E9Fd0aB182DD2Ac066Ce3AF7", target: "_blank" }),
+
     el("h2", "떡 NFT"),
     el(
       "p",
