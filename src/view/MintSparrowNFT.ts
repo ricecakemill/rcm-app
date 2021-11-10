1cada10b1fc99ad46b7cb1566314abc0e665d448
Hanul <hanul@hanul.me>
2021-11-11 01:06:38 +0900

    저작권 멘트 추가

--- src/view/MintSparrowNFT.ts
+++ src/view/MintSparrowNFT.ts
@@ -77,6 +77,7 @@ export default class MintSparrowNFT implements View {
                     },
                 }),
             ),
+            el("p", "* 무료 패러디물은 관계없으나, 판매를 목적으로 하는 경우는 저작권에 주의하세요."),
             el("p", "* 이미 발행된 NFT도 수정할 수 있도록 추후 기능 제공 예정"),
         ).appendTo(BodyNode);
     }
